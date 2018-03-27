7559241a5cfd7440e5b34697ac4318a38853fc25
Erik Frojdh <erik.frojdh@gmail.com>
2018-03-27 17:10:04 +0200

    meta

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DUSE_TEXTCLIENT=ON -DUSE_RECEIVER=ON -DUSE_GUI=OFF -DCMAKE_BUILD_TYPE=Release -DUSE_HDF5=OFF ..
+echo $PREFIX
+cmake .. \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DUSE_TEXTCLIENT=ON \
+      -DUSE_RECEIVER=ON \
+      -DUSE_GUI=OFF \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DUSE_HDF5=OFF\
+     
+
 cmake --build . -- -j8
 cmake --build . --target install
